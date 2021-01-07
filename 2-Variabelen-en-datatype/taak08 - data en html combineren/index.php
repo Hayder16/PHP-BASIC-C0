8f6ce64791aa52f93bb388fac197bcd7175a2c66
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-07 13:40:20 +0100

    data + html

---
+++ 2-Variabelen-en-datatype/taak08 - data en html combineren/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$voornaam1 = "Voornaam 1";
+$voornaam2 = "Voornaam 2";
+$voornaam3 = "Voornaam 3";
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>"
+?>
+</body>
+</hmtl>
