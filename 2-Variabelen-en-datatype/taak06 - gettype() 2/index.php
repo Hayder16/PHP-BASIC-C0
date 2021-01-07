bb5ae76afa62184d2c2333f9870f05ae74f23e32
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-07 12:52:41 +0100

    gettype2

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$name = "Hayder";
+echo gettype($name) . "<br>";
+$age = 17;
+echo gettype($age) . "<br>";
+$koek = 0.79;
+echo gettype($koek) . "<br>";
+$money = true;
+echo gettype($money) . "<br>";
+$moonla = null;
+echo gettype($moonla) . "<br>";
+?>
+</body>
+</html>
