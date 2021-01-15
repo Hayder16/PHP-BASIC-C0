20d7ba052c0a22a89fbe8a4207d81a2b4a7b6822
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-15 11:01:49 +0100

    array3

---
+++ 3-Arrays/taak03 - Array waarde toevoegen/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$myHeroes = ["Flash", "Spiderman", "Goku"];
+$myHeroes[3] = "Luffy";
+array_push($myHeroes, "Naruto");
+print_r($myHeroes);
+?>
+</body>
+</html>
