4f0d5865693b4526a7be4b24a16ac6d5f86f0f33
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-15 10:39:12 +0100

    array1

---
+++ 3-Arrays/taak01 - Array variabele/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$myMovies = ["<li>Coach Carter</li>", "<li>Spiderman</li>", "<li>Jumanji</li>"];
+echo $myMovies[0] . $myMovies[1] . $myMovies[2];
+?>
+</body>
+</html>
