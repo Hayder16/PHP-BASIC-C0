8c07f11e79b8eda385e4487f63b875257df34842
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-20 10:38:59 +0100

    multidimensionaal

---
+++ 3-Arrays/taak04 - Multidimensionaal/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$abcGetallen = ["A", "B", "C", "D", 24, 56, 78, 90];
+print_r($abcGetallen) . "<br>";
+echo $abcGetallen[0] . $abcGetallen[5] . "<br>";
+echo $abcGetallen[2] . $abcGetallen[7] . "<br>";
+?>
+</body>
+</html>
