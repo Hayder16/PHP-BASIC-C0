3cd4fa1623a86d07d52697cc7fff498940b21679
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-21 12:29:17 +0100

    taak1

---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$getal1 = 17;
+$getal2 = 8;
+echo $getal1 + $getal2 . "<br>";
+echo $getal1 - $getal2 . "<br>";
+echo $getal1 * $getal2 . "<br>";
+echo $getal1 / $getal2 . "<br>";
+?>
+</body>
+</html>
