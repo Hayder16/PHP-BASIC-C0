b704347c38fba3fe9fb7d1d0cebcc2ec640facac
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-21 12:40:16 +0100

    taak2

---
+++ 4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$getal1 = 10;
+echo $getal1 += 10 . "<br>";
+echo $getal1 -= 10 . "<br>";
+echo $getal1 *= 10 . "<br>";
+echo $getal1 /= 10 . "<br>";
+?>
+</body>
+</html>
