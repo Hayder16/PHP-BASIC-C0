0bb34e562214cf19f16611b4eb9e7610f2109bfd
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-01-22 11:08:58 +0100

    vardump

---
+++ 5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$test1 = 12;
+$test2 = "gewoon tekst";
+var_dump($test1 == $test2) . "<br>";
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
+var_dump();
+?>
+</body>
+</html>
