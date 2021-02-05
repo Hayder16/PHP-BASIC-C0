086ea2f5c4ef629a45f0da2f2a3a4827f82c0d57
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-02-05 10:58:53 +0100

    tellen

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function telAlleWoorden(){
+
+    $zin1 = strlen("Mijn blauwe auto is een mooie snelle wagen");
+    $lengteZin = $zin1;
+    echo $lengteZin;
+
+}
+
+telAlleWoorden();
+
+?>
+
+</body>
+</html>
