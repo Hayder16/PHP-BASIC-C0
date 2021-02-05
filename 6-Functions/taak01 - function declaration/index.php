cfc29db14169f4675aee3c24a0824c2eac1d89c1
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-02-05 10:37:16 +0100

    function

---
+++ 6-Functions/taak01 - function declaration/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+function zetTweeWoordenAanElkaar() {
+    $woordje = "Hallo";
+    $zinnetje = "mooie blauwe planeet";
+    $heleZin = $woordje . " " . $zinnetje;
+    echo $heleZin;
+}
+
+zetTweeWoordenAanElkaar();
+
+?>
+</body>
+</html>
