60ab58aca58909084a6f8b32dacc0016acfd9ff2
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-02-12 10:56:53 +0100

    return

--- 6-Functions/taak04 - return statement/index.php
+++ 6-Functions/taak04 - return statement/index.php
@@ -4,7 +4,15 @@
 
 <?php
 
+function geefTienProcent(){
+    $koopprijs = 75;
+    $korting = 10;
+    $kortingEuro = $koopprijs * $korting / 100;
+    $resultaat = $koopprijs - $kortingEuro;
 
+    return $resultaat;
+}
+echo geefTienProcent();
 
 ?>
 
